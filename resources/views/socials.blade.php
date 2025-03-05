ef3cf94f370358fc522e3e19608b46fe02b5b5d8
Cristian Araujo <a.cristianwz@gmail.com>
2025-03-05 14:06:51 -0300

    added option to open links in target blank && fix class selector

--- resources/views/socials.blade.php
+++ resources/views/socials.blade.php
@@ -1,11 +1,11 @@
-<div id="social-links">
+<div class="social-links">
     <ul>
         @if(config('laravel-share.showShareText', false))
         <li>{{ trans('laravel-share::translations.share') }}</li>
         @endif
         @foreach ($links as $provider => $url)
             <li>
-                <a href="{{ $url }}" class="social-button {{ $options['class'] ?? '' }}" id="{{ $options['id'] ?? '' }}" title="{{ $options['title'] ?? '' }}" rel="{{ $options['rel'] ?? '' }}"><span class="{{ $icons[$provider] }}"></span></a>
+                <a href="{{ $url }}" class="social-button {{ $options['class'] ?? '' }}" id="{{ $options['id'] ?? '' }}" title="{{ $options['title'] ?? '' }}" rel="{{ $options['rel'] ?? '' }}" target="{{ config('laravel-share.link_target', '_self') }}"><span class="{{ $icons[$provider] }}"></span></a>
             </li>
         @endforeach
     </ul>
